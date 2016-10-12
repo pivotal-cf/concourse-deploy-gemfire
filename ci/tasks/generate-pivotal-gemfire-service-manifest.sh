fb8d7606e23055e2e7f337687f172834beaf223b
John Calabrese <jcalabrese@pivotal.io>
2016-10-12 17:25:55 -0400

    [#131671985] adding required flag to manifest generator script

--- ci/tasks/generate-pivotal-gemfire-service-manifest.sh
+++ ci/tasks/generate-pivotal-gemfire-service-manifest.sh
@@ -15,6 +15,7 @@ omg-cli/omg-linux deploy-product \
   --ssl-ignore \
   $PRODUCT_PLUGIN \
   --deployment-name $DEPLOYMENT_NAME \
+  --server-instance-count 0
   --stemcell-ver $STEMCELL_VERSION > manifest/deployment.yml
 
 #eof
