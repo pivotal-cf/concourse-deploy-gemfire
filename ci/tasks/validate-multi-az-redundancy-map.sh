fa46fff54511b5ef24d6e99a80ac06944ac1e1d8
John Calabrese <jcalabrese@pivotal.io>
2016-10-18 14:48:08 -0400

    [#132625503] we need to install the jre as well for gfsh to work

--- ci/tasks/validate-multi-az-redundancy-map.sh
+++ ci/tasks/validate-multi-az-redundancy-map.sh
@@ -3,3 +3,5 @@ ls -lha
 echo $OMG_LOCATOR_STATIC_IP
 wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/1753/product_files/4635/download
 sudo dpkg -i pivotal-gemfire.deb
+sudp apt-get -y install jre
+gfsh version
