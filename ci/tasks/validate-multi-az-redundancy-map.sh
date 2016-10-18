2106980131fffeae06c95a000520ccf59e8db84d
John Calabrese <jcalabrese@pivotal.io>
2016-10-18 14:41:51 -0400

    [#132625503] direct pull from pivnet using curl

--- ci/tasks/validate-multi-az-redundancy-map.sh
+++ ci/tasks/validate-multi-az-redundancy-map.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 ls -lha
 echo $OMG_LOCATOR_STATIC_IP
-chmod + gfsh
-cd ./gfsh
-ls -lha
-sudo dpkg -i *.deb
+wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/1753/product_files/4635/download
+sudo dpkg -i pivotal-gemfire.deb
