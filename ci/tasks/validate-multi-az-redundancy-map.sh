0e48b25b65ce8ed14f5b7f64e68a211be22fb4a4
John Calabrese <jcalabrese@pivotal.io>
2016-10-18 14:51:40 -0400

    [#132625503] adding apt-get update and proper jre install command

--- ci/tasks/validate-multi-az-redundancy-map.sh
+++ ci/tasks/validate-multi-az-redundancy-map.sh
@@ -3,5 +3,6 @@ ls -lha
 echo $OMG_LOCATOR_STATIC_IP
 wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/1753/product_files/4635/download
 sudo dpkg -i pivotal-gemfire.deb
-sudo apt-get -y install jre
+sudo apt-get update
+sudo apt-get -y install default-jre
 gfsh version
