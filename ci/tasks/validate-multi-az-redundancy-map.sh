4c6fadb9c8987806f2ede42be3899e341b177849
John Calabrese <jcalabrese@pivotal.io>
2016-10-19 11:56:24 -0400

    [#132625503] adding test to check for all AZs mapped to redundancy zones on cache servers

--- ci/tasks/validate-multi-az-redundancy-map.sh
+++ ci/tasks/validate-multi-az-redundancy-map.sh
@@ -9,8 +9,25 @@ gfsh \
 -e "connect --locator=${LOCATOR_CONNECTION}" \
 -e "list members" \
 -e "create region --name=testing --redundant-copies=1 --type=PARTITION_REDUNDANT" \
+-e "put --key=('1') --value=('A') --region=testing" \
+-e "put --key=('2') --value=('B') --region=testing" \
+-e "put --key=('3') --value=('C') --region=testing" \
+-e "put --key=('4') --value=('D') --region=testing" \
+-e "put --key=('5') --value=('E') --region=testing" \
+-e "put --key=('6') --value=('F') --region=testing" \
+-e "put --key=('7') --value=('G') --region=testing" \
+-e "put --key=('8') --value=('H') --region=testing" \
+-e "describe config --member=cacheserver-0" \
+-e "describe config --member=cacheserver-1" \
+-e "describe config --member=cacheserver-2" \
+-e "describe config --member=cacheserver-3" \
+-e "describe config --member=cacheserver-4" \
+-e "describe config --member=cacheserver-5" \
 -e "show metrics --categories=partition --region=testing" > gemfire-output.txt
 cat gemfire-output.txt
-# checks redundancy in regions using the suggested method from pivotal docs: 
+# checks redundancy in regions using the suggested method from pivotal docs:
 # http://gemfire.docs.pivotal.io/docs-gemfire/latest/developing/partitioned_regions/checking_region_redundancy.html
-cat gemfire-output.txt | grep numBucketsWithoutRedundancy | grep "| 0"
+cat gemfire-output.txt | grep numBucketsWithoutRedundancy | grep "| 0" &&
+cat gemfire-output.txt | grep redundancy-zone | grep ": AZ1" &&
+cat gemfire-output.txt | grep redundancy-zone | grep ": AZ2" &&
+cat gemfire-output.txt | grep redundancy-zone | grep ": AZ3"
