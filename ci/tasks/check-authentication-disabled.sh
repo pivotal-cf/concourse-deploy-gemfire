5b8e6c52a7fc0456531ee7e81daaa44bc64412a9
John Calabrese <jcalabrese@pivotal.io>
2016-10-28 14:12:03 -0400

    [#133187961] updating test script

--- ci/tasks/check-authentication-disabled.sh
+++ ci/tasks/check-authentication-disabled.sh
@@ -2,6 +2,8 @@
 ls -lha
 sudo apt-get update
 sudo apt-get -y install default-jre-headless 
-cd gemfire-testapp
+sudo apt-get -y --no-install-recommends install default-jdk
+sudo apt-get -y install maven2
+cd gemfire-testapp/CMSClient
 ./mvnw clean package -DskipTests=true
 java -jar target/CMSClient-0.0.1-SNAPSHOT.jar --gemfire.locator.host=${LOCATOR_TEST_IP} —gemfire.locator.port=${LOCATOR_TEST_PORT}
